ba9ac5dd01c5e954de59eb5cad8269e16da63d13
tszkla2 <tom_sz@comcast.net>
2021-10-08 11:00:29 -0500

    got various colors to show in console

---
+++ dollars_bank_atm.js
@@ -0,0 +1,9 @@
+var readline = require('readline');
+var colors = require('colors');
+
+console.log('red'.red);
+console.log('yellow'.yellow);
+console.log('green'.green);
+console.log('blue'.blue);
+console.log('magenta'.magenta);
+console.log('white'.white);
